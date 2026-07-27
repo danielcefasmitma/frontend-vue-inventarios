c85236aa6ba9d68183ea0258a930f8e8620fee10
danielcefasmitma <cefasdaniel87@gmail.com>
2026-07-27 17:43:17 -0400

    template vue

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,9 +1,44 @@
 <template>
+    <!--
     <h1>App Layout</h1>
     <RouterLink to="/admin">PANEL ADMINISTRATIVO</RouterLink>|
     <RouterLink to="/admin/perfil">MI PERFIL</RouterLink>|
     <RouterLink to="/admin/usuario">USUARIOS</RouterLink>|
     <RouterLink to="/">VOLVER A LA PAGINA DE INICIO</RouterLink>
-
     <router-view/>
+    -->
+    
+
+    <div class="layout-wrapper" :class="containerClass">
+        <AppTopbar />
+        <AppSidebar />
+        <div class="layout-main-container">
+            <div class="layout-main">
+                <router-view />
+            </div>
+            <AppFooter />
+        </div>
+        <div class="layout-mask animate-fadein" @click="hideMobileMenu" />
+    </div>
+    <Toast />
 </template>
+
+<script setup>
+import { useLayout } from '@/layout/composables/layout';
+import { computed } from 'vue';
+import AppFooter from './AppFooter.vue';
+import AppSidebar from './AppSidebar.vue';
+import AppTopbar from './AppTopbar.vue';
+
+const { layoutConfig, layoutState, hideMobileMenu } = useLayout();
+
+const containerClass = computed(() => {
+    return {
+        'layout-overlay': layoutConfig.menuMode === 'overlay',
+        'layout-static': layoutConfig.menuMode === 'static',
+        'layout-overlay-active': layoutState.overlayMenuActive,
+        'layout-mobile-active': layoutState.mobileMenuActive,
+        'layout-static-inactive': layoutState.staticMenuInactive
+    };
+});
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import tailwindcss from '@tailwindcss/vite';
+import Components from 'unplugin-vue-components/vite';
+import { fileURLToPath, URL } from 'node:url';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  optimizeDeps: {
+        noDiscovery: true
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+            resolvers: [PrimeVueResolver()]
+        })
+  ],
+  resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+  },
+  css: {
+        preprocessorOptions: {
+            scss: {
+              api:'modern-compiler'
+            }
+        }
+  }
 })
